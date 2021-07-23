48cffafdd4732eda95123291318ca7b62fcfcda2
DanhVT <tintin.thanhdanh@gmail.com>
2021-07-23 12:59:33 +0700

    production build

--- back/server.js
+++ back/server.js
@@ -39,6 +39,13 @@ app.use(function (req, res, next) {
   }
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', appConfig.crossDomain);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use('/api', router());
 
 app.get('*', (req, res) => {
