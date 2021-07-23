6eb31dac3185803d543c77dc1adeba68630e9e21
DanhVT <tintin.thanhdanh@gmail.com>
2021-07-23 15:53:46 +0700

    fix css for mobile

---
+++ back/app/public/precache-manifest.3994a1d256a3c2da373e16de50cbc363.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2416fcb1e994186705",
+    "url": "/css/app.9a842548.css"
+  },
+  {
+    "revision": "8711a68e53ce10c0ca66869139605687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e2416fcb1e994186705",
+    "url": "/js/app.2d55511f.js"
+  },
+  {
+    "revision": "ee1fa8b802815a183fd6",
+    "url": "/js/chunk-1cabc97d.ac7243e9.js"
+  },
+  {
+    "revision": "46c86e25c40953a98f9d",
+    "url": "/js/chunk-2d2086b7.aadacf2e.js"
+  },
+  {
+    "revision": "c1d785caa053a6afa969",
+    "url": "/js/chunk-2d208bfd.941ea75a.js"
+  },
+  {
+    "revision": "f580a855cd9374aa6c6d",
+    "url": "/js/chunk-vendors.7c1e140e.js"
+  },
+  {
+    "revision": "248764d12875a71ef3ae4ea0a2d9070f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
